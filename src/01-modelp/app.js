6f906589a37669f8a5625816a94d5016a269dd7d
gugray <gabor@berlin.de>
2023-10-27 12:55:40 +0200

    animation update from function in live.js

--- src/01-modelp/app.js
+++ src/01-modelp/app.js
@@ -17,7 +17,7 @@ import {RGBShiftShader} from "three/addons/shaders/RGBShiftShader.js";
 // -- Move lights? add spotlights? add bloom filter?
 // -- Add yellow BG lines?
 // OK Show volume
-// -- Move non-curl update into web worker too
+// XX Move non-curl update into web worker too
 // -- Ease into new states
 // -- Neater transitions between simulation and model
 // -- Neater maxAge changes
@@ -25,7 +25,6 @@ import {RGBShiftShader} from "three/addons/shaders/RGBShiftShader.js";
 
 // https://sketchfab.com/3d-models/tonatiuh-9db1f3a422c149ceade14a9c294d4e8a
 const modelUrl = "data/tonatiuh-32k.ply";
-const modelScale = 36;
 
 const mat = new THREE.Matrix4();
 mat.makeRotationY(Math.PI * 0.5);
@@ -35,6 +34,7 @@ const shadowMapSz = 4096;
 const shadowCamDim = 40;
 
 const ctrl = {
+  modelScale: 36,
   useEffectsComposer: false,
   preserveBuffer: false,
   pulseSize: false,
@@ -47,8 +47,12 @@ const ctrl = {
 };
 
 const startTime = Date.now();
+let lastTime = startTime;
+
 const state = {
-  lastTime: startTime,
+  time: 0,
+  dT: 0,
+  funnyTime: 0,
   lo: 0,
   mid: 0,
   hi: 0,
@@ -77,6 +81,8 @@ async function getLive() {
   dyn = live.dyn;  
   live.updateCtrl(ctrl);
   setGain(ctrl.gain);
+  if (dirLight1) dirLight1.castShadow = ctrl.useShadow;
+  if (dirLight2) dirLight2.castShadow = ctrl.useShadow;
   updateUpdaters();
 }
 await getLive();
@@ -140,17 +146,15 @@ if (ctrl.useEffectsComposer) {
 function makeDirLight(x, y, z, intensity) {
   const light = new THREE.DirectionalLight(0xffffff, intensity);
   light.position.set(x, y, z);
-  if (ctrl.useShadow) {
-    light.castShadow = true;
-    light.shadow.camera.top = shadowCamDim;
-    light.shadow.camera.left = -shadowCamDim;
-    light.shadow.camera.bottom = -shadowCamDim;
-    light.shadow.camera.right = shadowCamDim;
-    light.shadow.camera.near = 10;
-    light.shadow.camera.far = 500;
-    light.shadow.mapSize.set(shadowMapSz, shadowMapSz);
-    light.shadow.radius = 4;
-  }
+  light.shadow.camera.top = shadowCamDim;
+  light.shadow.camera.left = -shadowCamDim;
+  light.shadow.camera.bottom = -shadowCamDim;
+  light.shadow.camera.right = shadowCamDim;
+  light.shadow.camera.near = 10;
+  light.shadow.camera.far = 500;
+  light.shadow.mapSize.set(shadowMapSz, shadowMapSz);
+  light.shadow.radius = 4;
+  light.castShadow = ctrl.useShadow;
   return light;
 }
 
@@ -190,14 +194,7 @@ model.putAllOnModel();
 for (let ix = 0; ix < model.count; ++ix)
   model.setPointAge(ix, Math.floor(ctrl.maxAge * Math.random()));
 noise.seed(Math.random());
-updateFromModel(0);
-
-function rotateTmpObjToNrm() {
-  perm.obj.rotation.z = Math.atan2(perm.nrm.y, perm.nrm.x);
-  perm.hor.set(perm.nrm.x, perm.nrm.y, 0).normalize();
-  perm.obj.rotation.y = -Math.atan2(perm.hor.z, perm.hor.x);
-  perm.obj.rotation.x = -Math.atan2(perm.nrm.dot(perm.unitZ), perm.nrm.dot(perm.nrm.clone().cross(perm.unitZ)));
-}
+// updateFromModel(0);
 
 function rotateTmpObjToNrm2() {
   const angle = perm.unitY.angleTo(perm.nrm);
@@ -216,7 +213,7 @@ function updateSimulation(dT) {
     perm.obj.scale.y = 1 + state.hi / 256;
     perm.obj.scale.x = perm.obj.scale.z = 1 + state.mid / 256;
 
-    perm.obj.position.set(perm.mpt.cx * modelScale, perm.mpt.cy * modelScale, perm.mpt.cz * modelScale);
+    perm.obj.position.set(perm.mpt.cx * ctrl.modelScale, perm.mpt.cy * ctrl.modelScale, perm.mpt.cz * ctrl.modelScale);
     perm.nrm.set(perm.mpt.vx, perm.mpt.vy, perm.mpt.vz);
     perm.nrm.normalize();
     rotateTmpObjToNrm2();
@@ -228,39 +225,6 @@ function updateSimulation(dT) {
   }
 }
 
-function updateFromModel(time) {
-
-  mesh.rotation.y = Math.sin(time * 0.0002) * 0.3;
-
-  for (let i = 0; i < model.count; ++i) {
-    model.getPoint(i, perm.mpt);
-    perm.obj.position.set(perm.mpt.cx * modelScale, perm.mpt.cy * modelScale, perm.mpt.cz * modelScale);
-
-    perm.nrm.set(perm.mpt.nx, perm.mpt.ny, perm.mpt.nz);
-    rotateTmpObjToNrm();
-
-    // perm.obj.rotation.x = Math.PI * 0.25;
-    // perm.obj.rotation.y = Math.PI * 0.25;
-    //
-    // perm.obj.rotation.y =
-    //   Math.sin(perm.mpt.cx / 4 + time * 0.0005) +
-    //   Math.sin(perm.mpt.cy / 4 + time * 0.0005) +
-    //   Math.sin(perm.mpt.cz / 4 + time * 0.0005);
-    // perm.obj.rotation.x = perm.obj.rotation.y * 0.5;
-
-    if (ctrl.pulseSize) {
-      perm.obj.scale.y = 1 + Math.sin(time * 0.001) * 1;
-    }
-    perm.obj.scale.y = 1 + state.hi / 256;
-    perm.obj.scale.x = perm.obj.scale.z = 1 + Math.sqrt(state.mid) / 16;
-
-    perm.obj.updateMatrix();
-    mesh.setMatrixAt(i, perm.obj.matrix);
-    perm.clr.set(perm.mpt.r / 64, perm.mpt.g / 64, perm.mpt.b / 64);
-    mesh.setColorAt(i, perm.clr);
-  }
-}
-
 function animate() {
   requestAnimationFrame(animate);
 
@@ -269,6 +233,12 @@ function animate() {
   else [state.lo, state.mid, state.hi, state.vol] = [0, 0, 0, 0];
   const volPercent = (state.vol / 2048 * 100).toFixed(2);
   elmVolumeVal.style.height = volPercent + "%";
+
+  const now = Date.now();
+  state.time = now - startTime;
+  state.dT = now - lastTime;
+  state.funnyTime += state.dT + state.vol * 0.1;
+  lastTime = now;
 
   camRotSpeed.add(camRotAccel);
   camPanSpeed.add(camPanAccel);
@@ -286,12 +256,11 @@ function animate() {
   if (Math.abs(camPanSpeed.x) < 0.0001) camPanSpeed.x = 0;
   if (Math.abs(camPanSpeed.z) < 0.0001) camPanSpeed.z = 0;
 
-  const now = Date.now();
-  if (ctrl.flowSim) updateSimulation(now - state.lastTime);
-  else updateFromModel(now - startTime);
+  if (ctrl.flowSim) updateSimulation(state.dT + state.vol);
+  else dyn.updateFromModel(perm, ctrl, state, model, mesh);
+
   mesh.instanceMatrix.needsUpdate = true;
   mesh.computeBoundingSphere();
-  state.lastTime = now;
 
   if (ctrl.useEffectsComposer) composer.render();
   else {
--- src/01-modelp/live.js
+++ src/01-modelp/live.js
@@ -11,7 +11,52 @@ function updateCtrl(ctrl) {
 
 const dyn = {
 
+  updateFromModel: (perm, ctrl, state, model, mesh) => {
+
+    const time = state.funnyTime;
+    mesh.rotation.y = Math.sin(state.time * 0.0002) * 0.3;
+
+    for (let i = 0; i < model.count; ++i) {
+      model.getPoint(i, perm.mpt);
+      perm.obj.position.set(perm.mpt.cx * ctrl.modelScale, perm.mpt.cy * ctrl.modelScale, perm.mpt.cz * ctrl.modelScale);
+
+      perm.nrm.set(perm.mpt.nx, perm.mpt.ny, perm.mpt.nz);
+      rotateTmpObjToNrm(perm);
+
+      // perm.obj.rotation.x = Math.PI * 0.25;
+      // perm.obj.rotation.y = Math.PI * 0.25;
+
+      perm.obj.rotation.y =
+        Math.sin(perm.mpt.cx / 4 + time * 0.0005) +
+        Math.sin(perm.mpt.cy / 4 + time * 0.0005) +
+        Math.sin(perm.mpt.cz / 4 + time * 0.0005);
+      // perm.obj.rotation.y *= 1 + state.mid * 0.001;
+      perm.obj.rotation.x = perm.obj.rotation.y * 0.5;
+
+      if (ctrl.pulseSize) {
+        perm.obj.scale.y = 1 + Math.sin(time * 0.001) * 1;
+      }
+      perm.obj.scale.set(1,1,1);
+      // perm.obj.scale.y = 1 + state.lo / 128;
+      // perm.obj.scale.x = perm.obj.scale.z = 1 + Math.sqrt(state.mid) / 16;
+
+      perm.obj.updateMatrix();
+      mesh.setMatrixAt(i, perm.obj.matrix);
+      perm.clr.set(perm.mpt.r / 64, perm.mpt.g / 64, perm.mpt.b / 64);
+      mesh.setColorAt(i, perm.clr);
+    }
+  },
+
 };
+
+
+function rotateTmpObjToNrm(perm) {
+  perm.obj.rotation.z = Math.atan2(perm.nrm.y, perm.nrm.x);
+  perm.hor.set(perm.nrm.x, perm.nrm.y, 0).normalize();
+  perm.obj.rotation.y = -Math.atan2(perm.hor.z, perm.hor.x);
+  perm.obj.rotation.x = -Math.atan2(perm.nrm.dot(perm.unitZ), perm.nrm.dot(perm.nrm.clone().cross(perm.unitZ)));
+}
+
 
 ///return {
 ///  updateCtrl,
