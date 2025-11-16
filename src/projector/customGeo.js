defe323ef9a52ffd13856148ab27a966d5ce8103
gugray <gabor@berlin.de>
2025-11-16 18:01:52 +0100

    removed code texture map from boxes

--- src/projector/customGeo.js
+++ src/projector/customGeo.js
@@ -16,8 +16,8 @@ export function hackBoxMaterial(geo, mat) {
     // This comes first in code => we'll use mapColor later.
     // prettier-ignore
     shader.fragmentShader = shader.fragmentShader.replace("#include <map_fragment>", `
-float alpha = 0.4;
-diffuseColor.rgb *= 2.0;
+float alpha = 1.0;
+// diffuseColor.rgb *= 2.0;
 // diffuseColor whatevs
     `);
     // prettier-ignore
@@ -26,16 +26,4 @@ diffuseColor.rgb *= 2.0;
 gl_FragColor.a = alpha;
     `);
   };
-
-  const uvAttribute = geo.getAttribute("uv");
-  // prettier-ignore
-  const uvMap = [
-    0, 0.75, 0, 1, 1, 0.75, 1, 1,
-    0, 0.25, 0, 0.5, 1, 0.25, 1, 0.5,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0.5, 0, 0.75, 1, 0.5, 1, 0.75,
-    0, 0, 0, 0.25, 1, 0, 1, 0.25,
-  ];
-  for (let i = 0; i < 24; ++i) uvAttribute.setXY(i, uvMap[2 * i], uvMap[2 * i + 1]);
 }
